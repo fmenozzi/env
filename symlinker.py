ea83d1745f8cf3bb6d33b49c7f0ee63f6abd6d48
Federico Menozzi <federicogmenozzi@gmail.com>
2022-06-01 17:01:52 -0700

    Rename symlinks.ini to links.ini.
    This way I don't have to tab through to open the symlinker.py script.

--- symlinker.py
+++ symlinker.py
@@ -66,7 +66,7 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('command', choices=['link', 'clean'], help='Command to run')
-    parser.add_argument('-c', '--config', help='Config file path', default='symlinks.ini')
+    parser.add_argument('-c', '--config', help='Config file path', default='links.ini')
     args = parser.parse_args()
 
     main(args)
