970c25cd23903b649c0f5dbb5151c86908326d77
Federico Menozzi <federicogmenozzi@gmail.com>
2022-06-08 18:29:55 -0700

    Clean up dotfiles/env dir paths.

--- dotfiles/bash/fzf.bash
+++ dotfiles/bash/fzf.bash
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == *$DOTFILES/nvim/plugged/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}$DOTFILES/nvim/plugged/fzf/bin"
+if [[ ! "$PATH" == *$DOTFILES_PATH/nvim/plugged/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}$DOTFILES_PATH/nvim/plugged/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "$DOTFILES/nvim/plugged/fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "$DOTFILES_PATH/nvim/plugged/fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "$DOTFILES/nvim/plugged/fzf/shell/key-bindings.bash"
+source "$DOTFILES_PATH/nvim/plugged/fzf/shell/key-bindings.bash"
