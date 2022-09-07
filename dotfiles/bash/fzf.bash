8b769cbd32de03d43e5c6c743ab47846f196c3ce
Federico Menozzi <federicogmenozzi@gmail.com>
2022-09-06 18:14:08 -0700

    Update env for arch.
    * Added "at" and "signal-desktop" packages and alphabetized archpkgs
    * fdfind -> fd
    * Updated kitty font config
        * Now relying on nerd-fonts-jetbrains-mono AUR package to install
          jetbrains nerd font, though for whatever reason the font family is
          different with this method compared to getnf
    * Updated startpage font to jetbrains mono nerd font
    * Updated open tool and ran it through shellcheck
        * Added some TODOs for future work to improve it
    * Updated update tool

--- dotfiles/bash/fzf.bash
+++ dotfiles/bash/fzf.bash
@@ -12,6 +12,6 @@ fi
 # ------------
 source "$DOTFILES_PATH/nvim/plugged/fzf/shell/key-bindings.bash"
 
-export FZF_DEFAULT_COMMAND="fdfind . $HOME"
+export FZF_DEFAULT_COMMAND="fd . $HOME"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_ALT_C_COMMAND="fdfind -t d . $HOME"
+export FZF_ALT_C_COMMAND="fd -t d . $HOME"
