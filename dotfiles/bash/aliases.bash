2713ba2e1c4a9b84343b0efe74e09369d72d6bd2
Federico Menozzi <federicogmenozzi@gmail.com>
2022-11-17 21:39:07 -0800

    Add "ex" alias for extracting from archives.

--- dotfiles/bash/aliases.bash
+++ dotfiles/bash/aliases.bash
@@ -21,6 +21,26 @@ alias l='ls -CF'
 alias lc='wc -l'
 alias o='vim $(fzf)'
 
+# Extract from various archive types
+extract() {
+  if [ -f "$1" ]; then
+    case "$1" in
+      *.tar.bz2)   tar xjf "$1"   ;;
+      *.tar.gz)    tar xzf "$1"   ;;
+      *.tar.xz)    tar xJf "$1"   ;;
+      *.bz2)       bunzip2 "$1"   ;;
+      *.gz)        gunzip "$1"    ;;
+      *.tar)       tar xf "$1"    ;;
+      *.tbz2)      tar xjf "$1"   ;;
+      *.tgz)       tar xzf "$1"   ;;
+      *.zip)       unzip "$1"     ;;
+      *)           echo "'$1' cannot be extracted via ex()" ;;
+    esac
+  else
+    echo "'$1' is not a valid file"
+  fi
+}
+
 # Enable color support of ls and also add handy aliases
 if [ -x /usr/bin/dircolors ]; then
     test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
