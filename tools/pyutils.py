f5233e54c837b8403901849db77b9436f45fc441
Federico Menozzi <federicogmenozzi@gmail.com>
2023-03-03 10:05:11 -0500

    Add fzf function to pyutils.
    Since we use fzf quite a lot across various tools, seems worth it to
    create a dedicated function for it.

--- tools/pyutils.py
+++ tools/pyutils.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 
-from subprocess import run, PIPE
+from subprocess import run, PIPE, check_output, CalledProcessError
 
 def sh(cmd):
     res = run(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True, text=True)
     stdout = res.stdout.strip() if res.stdout else None
     stderr = res.stderr.strip() if res.stderr else None
     return stdout, stderr, res.returncode
+
+def fzf(input_items, fzf_args_str):
+    input_bytes = '\n'.join(input_items).encode('utf-8')
+    try:
+        output = check_output(f'fzf {fzf_args_str}', shell=True, input=input_bytes)
+    except CalledProcessError as e:
+        # fzf will return 130 if nothing is chosen; output will still be empty.
+        output = e.output
+    return output.decode('utf-8').strip()
