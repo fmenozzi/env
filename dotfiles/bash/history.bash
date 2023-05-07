40bd14639a9b85f803628b8f0706bb86c80ae0bd
Federico Menozzi <federicogmenozzi@gmail.com>
2023-05-07 07:08:47 -0400

    Increase bash history size, update formatting, etc.

--- dotfiles/bash/history.bash
+++ dotfiles/bash/history.bash
@@ -4,5 +4,9 @@ HISTCONTROL=ignoreboth
 # Append to history file instead of overwriting it.
 shopt -s histappend
 
-HISTSIZE=1000
-HISTFILESIZE=2000
+HISTSIZE=10000000
+HISTFILESIZE=10000000
+HISTTIMEFORMAT="%FT%R "
+
+# Append history lines from this session to the history file.
+export PROMPT_COMMAND="history -a;$PROMPT_COMMAND"
