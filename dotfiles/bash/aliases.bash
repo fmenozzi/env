ef282a371f597a2fd899768e6808dd7dae1ced5f
Federico Menozzi <federicogmenozzi@gmail.com>
2023-06-27 17:59:23 -0400

    Update alert alias urgency to normal.

--- dotfiles/bash/aliases.bash
+++ dotfiles/bash/aliases.bash
@@ -61,7 +61,7 @@ fi
 
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
-alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+alias alert='notify-send --urgency=normal -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
 # Command substitutions
 alias tmux='tmux -2'
