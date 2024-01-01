624c1a8cdf2bae148d803982c3b8a7a7c03e3273
Federico Menozzi <federicogmenozzi@gmail.com>
2024-01-01 13:28:22 -0500

    Overhaul backup scripts.
    This commit introduces a new `rbackup` script that performs a backup to
    a restic repository. The script expects a bash config file as an
    argument, whose job is to provide BACKUP_INCLUDE and optionally
    BACKUP_EXCLUDE and RESTIC_PASSWORD variables. This allows to more easily
    say "back up these things (but not these other things) to this repo",
    without having to create a homegrown script for every backup scenario.
    For example, it's now much easier to backup to a repo on an external
    drive once it's mounted.
    
    The existing `homebackup` and `bwbackup` scripts are kept for
    convenience, as those are the most common backup scenarios, but
    `homebackup` now simply calls out to `rbackup` to do the actual local
    and remote backups.
    
    `bwbackup` is largely unchanged in this respect though, as we use restic
    slightly differently there (i.e. piping bw vault data directly into
    restic), but a future commit might allow for similar parameterization.

---
+++ backup/ava_home.bash
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+RESTIC_PASSWORD="$(pass ava/RESTIC_PASSWORD)"
+
+BACKUP_INCLUDE=(
+    /home/fed/archive/
+    /home/fed/arduino/
+    /home/fed/aseprite/
+    /home/fed/audio/
+    /home/fed/aur/
+    /home/fed/db/
+    /home/fed/desktop/
+    /home/fed/documents/
+    /home/fed/env/
+    /home/fed/freecad/
+    /home/fed/images/
+    /home/fed/kicad/
+    /home/fed/luks/
+    /home/fed/printer/
+    /home/fed/projects/
+    /home/fed/scratch/
+    /home/fed/syncthing/
+    /home/fed/videos/
+    /home/fed/LIFE_TODO
+    /home/fed/TODO
+    /home/fed/vault.gpgtar
+    /home/fed/.bash_history
+    /home/fed/.config/
+    /home/fed/.gnupg/
+    /home/fed/.local/
+    /home/fed/.password-store/
+    /home/fed/.python_history
+    /home/fed/.ssh/
+    /home/fed/.sqlite_history
+)
+
+BACKUP_EXCLUDE=(
+    /home/fed/env/dotfiles/nvim/plugged/
+    /home/fed/.local/share/Steam/
+    /home/fed/.local/share/Trash
+)
+
+export RESTIC_PASSWORD BACKUP_INCLUDE BACKUP_EXCLUDE
+
+# Clean home directory first to ensure we don't back up e.g. build artifacts.
+clean /home/fed/
+
+# Lock vault before backup.
+vault lock
