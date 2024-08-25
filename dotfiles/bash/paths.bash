6a536f586d9ac84717e0a9d33d3870914208d3f1
Federico Menozzi <federicogmenozzi@gmail.com>
2024-08-25 15:12:16 -0400

    Use bash array for PATH elements.

--- dotfiles/bash/paths.bash
+++ dotfiles/bash/paths.bash
@@ -3,4 +3,11 @@
 export GOPATH=~/.go
 export RUST_SRC_PATH=~/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src/
 
-export PATH=$PATH:$DOTFILES_PATH/git/custom-commands:$ENV_PATH/scripts:~/.cargo/bin:$GOPATH/bin
+paths=(
+  $DOTFILES_PATH/git/custom-commands
+  $ENV_PATH/scripts
+  $HOME/.cargo/bin
+  $GOPATH/bin
+)
+
+export PATH=$PATH:$(IFS=':'; printf '%s' "${paths[*]}")
