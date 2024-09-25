dbf811924933055fde0f1965407beccba3e587a5
Federico Menozzi <federicogmenozzi@gmail.com>
2024-09-25 08:32:08 -0400

    Add restic backup config for roy dotfiles.
    For now all I care about are things like ssh/pgp keys and history files,
    since the "main" files are synced from ava and are backed up on her end
    for now.

---
+++ backup/roy_home.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+RESTIC_PASSWORD="$(pass roy/RESTIC_PASSWORD)"
+
+BACKUP_INCLUDE=(
+    /home/fed/.bash_history
+    /home/fed/.config/
+    /home/fed/.gnupg/
+    /home/fed/.password-store/
+    /home/fed/.ssh
+)
+
+BACKUP_EXCLUDE=()
+
+export RESTIC_PASSWORD BACKUP_INCLUDE BACKUP_EXCLUDE
