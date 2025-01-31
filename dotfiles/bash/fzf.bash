c34c0000706c5d3e5e1ef550e913f3a56a487ace
Federico Menozzi <federicogmenozzi@gmail.com>
2025-01-31 08:24:19 -0500

    Use fzf's built-in shell integration scripts.

--- dotfiles/bash/fzf.bash
+++ dotfiles/bash/fzf.bash
@@ -1,18 +1,6 @@
 #!/usr/bin/env bash
 
-# Setup fzf
-# ---------
-if [[ ! "$PATH" == *$DOTFILES_PATH/nvim/plugged/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}$DOTFILES_PATH/nvim/plugged/fzf/bin"
-fi
-
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "$DOTFILES_PATH/nvim/plugged/fzf/shell/completion.bash" 2> /dev/null
-
-# Key bindings
-# ------------
-source "$DOTFILES_PATH/nvim/plugged/fzf/shell/key-bindings.bash"
+source <(fzf --bash)
 
 export FZF_DEFAULT_COMMAND="fd . ."
 export FZF_CTRL_T_COMMAND="cat <(fd . $HOME) <(fd . /mnt/ava-raid)"
