c21b932b8a28f4bb6343cc7ac1ac45a5202dcd91
Federico Menozzi <federicogmenozzi@gmail.com>
2025-05-12 07:25:37 -0400

    Add `~/.bin` to PATH.

--- dotfiles/bash/paths.bash
+++ dotfiles/bash/paths.bash
@@ -4,6 +4,7 @@ export GOPATH=~/.go
 export RUST_SRC_PATH=~/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src/
 
 paths=(
+  $HOME/.bin
   $DOTFILES_PATH/git/custom-commands
   $ENV_PATH/scripts
   $HOME/.cargo/bin
