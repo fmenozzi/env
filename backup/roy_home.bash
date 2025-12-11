e599770b6929f49bac88214f42b5938d7a85d4c7
Federico Menozzi <federicogmenozzi@gmail.com>
2025-12-11 07:59:41 -0500

    Add some more includes/excludes to roy_home backup.
    Also run the same clean/vault lock commands as ava.

--- backup/roy_home.bash
+++ backup/roy_home.bash
@@ -5,6 +5,8 @@ set -euo pipefail
 RESTIC_PASSWORD="$(pass roy/RESTIC_PASSWORD)"
 
 BACKUP_INCLUDE=(
+    /home/fed/env/
+    /home/fed/vault.gpgtar
     /home/fed/.bash_history
     /home/fed/.config/
     /home/fed/.gnupg/
@@ -12,6 +14,14 @@ BACKUP_INCLUDE=(
     /home/fed/.ssh
 )
 
-BACKUP_EXCLUDE=()
+BACKUP_EXCLUDE=(
+    /home/fed/env/dotfiles/nvim/plugged/
+)
 
 export RESTIC_PASSWORD BACKUP_INCLUDE BACKUP_EXCLUDE
+
+# Clean home directory first to ensure we don't back up e.g. build artifacts.
+clean /home/fed/
+
+# Lock vault before backup.
+vault lock
